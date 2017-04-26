2e73b953d75fd15b0082072d351f92211f1e8f8a
Turan Mahmudov <turan.mahmudov@gmail.com>
2017-04-26 21:25:15 +0400

    Fix launcher

--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-exec $SNAP/bin/instagraph "$@"
+if [ "$SNAP_ARCH" = "amd64" ]; then
+    ARCH="x86_64-linux-gnu"
+elif [ "$SNAP_ARCH" = "armhf" ]; then
+    ARCH="arm-linux-gnueabihf"
+elif [ "$SNAP_ARCH" == "arm64" ]; then
+    ARCH="aarch64-linux-gnu"
+else
+    ARCH="$SNAP_ARCH-linux-gnu"
+fi
+
+exec $SNAP/lib/$ARCH/bin/Instagraph "$@"
